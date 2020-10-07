dfccca1b3c9c671d74509b7d3c4137fba0d527b3
Cho Junseo <chojuneseo1@gmail.com>
2020-10-08 08:58:16 +0900

    Auto stash before rebase of "origin/main"

---
+++ ChangeColor.js
@@ -0,0 +1,18 @@
+function changeColor(color){
+    document.querySelector('container').style.backgroundColor='white';
+
+
+}
+
+function backgroundColorChanger(){
+    document.getElementById('container').style.backgroundColor='blue';
+    
+        
+
+}
+
+
+function backgroundColorChanger1(){
+    document.getElementById('container').style.backgroundColor='red';
+    
+}
