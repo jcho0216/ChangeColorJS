81fc54f92a2288cb7aa7df2aa9f7031b1b3e3e70
Cho Junseo <chojuneseo1@gmail.com>
2020-10-22 10:56:36 +0900

    Intensified webpage clone coding

--- jQuery/animation.js
+++
@@ -1,9 +0,0 @@
-$('#hi').on('click', function(){
-    $('#hi').animate({marginLeft: '100px', marginTop:'100px'}, 2000)
-}); 
-
-
-
-$('#show-menu').on('click', function(){
-    $('.left-menu').animate({marginLeft: '-7px'});
-})
