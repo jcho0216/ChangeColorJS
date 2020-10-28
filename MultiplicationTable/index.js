61344bc2dc0876597b16f5b14e2ce83e684945a5
Cho Junseo <chojuneseo1@gmail.com>
2020-10-28 23:38:01 +0900

    계산기 완성되지않은 버젼

--- MultiplicationTable/index.js
+++ MultiplicationTable/index.js
@@ -1,14 +1,12 @@
-let num = prompt('구구단 몇 단을 볼까요?', '1~9의 숫자입력');
+let inp = document.forms['cals'];
+let input = inp.getElementsByTagName('input');
+let cls_btn = document.getElementsByClassName('cls_btn')[0];
+let result_btn = document.getElementsByClassName('result_btn')[0];
 
-if(num == '' || isNaN(num)){
-    alert('올바른 값을 입력하세요.');
-
-}
-else if(num < 1 || num > 10){
-    alert('1부터 9까지 입력할 수 있습니다.');
-}
-else{
-    for(let i = 1; i <= 9; i++){
-        document.write(num+ '*' + i + '=' + num * i + '<br>');
+for(let i = 0; i < input.length;i++){
+    if(input[i].value != '=' && input[i].value != 'clear'){
+        input[i].onclick = function(){
+            cals(this.value);
+        }
     }
 }
